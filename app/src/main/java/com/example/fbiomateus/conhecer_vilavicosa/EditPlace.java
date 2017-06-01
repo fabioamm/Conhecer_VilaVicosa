6612338595a680525af992e5ba20b1b9c59428f5
fabioamm <fabio.amm.95@gmail.com>
2017-06-01 17:18:00 +0100

    area reservada

---
+++ app/src/main/java/com/example/fbiomateus/conhecer_vilavicosa/EditPlace.java
@@ -0,0 +1,94 @@
+package com.example.fbiomateus.conhecer_vilavicosa;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class EditPlace extends AppCompatActivity {
+
+    DBHelper dbHelper;
+    private Spinner sp1;
+    private EditText etName;
+    private EditText etDescription;
+    private EditText etOpenHour;
+    private EditText etCloseHour;
+    private EditText etContact;
+    private EditText etImgUrl;
+    private EditText etLatitude;
+    private EditText etLongitude;
+    private Button btnUpdate;
+    private Button btnDelete;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_place);
+        getSupportActionBar().setTitle("Editar/Apagar local");
+        findViews();
+        dbHelper = new DBHelper(this);
+
+        Intent intent = getIntent();
+        etName.setText(intent.getStringExtra("name"));
+        etDescription.setText(intent.getStringExtra("description"));
+        etOpenHour.setText(intent.getStringExtra("openHour"));
+        etCloseHour.setText(intent.getStringExtra("closeHour"));
+        etContact.setText(intent.getStringExtra("contact"));
+        etImgUrl.setText(intent.getStringExtra("imgUrl"));
+        etLatitude.setText(intent.getStringExtra("latitude"));
+        etLongitude.setText(intent.getStringExtra("longitude"));
+
+        this.btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    dbHelper.updatePlace(sp1.getSelectedItem().toString(), etName.getText().toString(), etDescription.getText().toString(), etOpenHour.getText().toString(), etCloseHour.getText().toString(), etContact.getText().toString(), etImgUrl.getText().toString(), etLatitude.getText().toString(), etLongitude.getText().toString());
+                    Context context = getApplicationContext();
+                    CharSequence text = etName.getText().toString() + " atualizado!";
+
+                    Toast toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+                    toast.show();
+                } catch(Exception e) {
+                    Log.d("ERROR", e.toString());
+                }
+            }
+        });
+
+        this.btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    dbHelper.deletePlace(etName.getText().toString());
+                    Context context = getApplicationContext();
+                    CharSequence text = etName.getText().toString() + " removido!";
+
+                    Toast toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+                    toast.show();
+                } catch(Exception e) {
+                    Log.d("ERROR", e.toString());
+                }
+            }
+        });
+    }
+
+    private void findViews() {
+        this.sp1 = (Spinner) findViewById(R.id.sp1);
+        this.etName = (EditText) findViewById(R.id.etName);
+        this.etDescription = (EditText) findViewById(R.id.etDescription);
+        this.etOpenHour = (EditText) findViewById(R.id.etOpenHour);
+        this.etCloseHour = (EditText) findViewById(R.id.etCloseHour);
+        this.etContact = (EditText) findViewById(R.id.etContact);
+        this.etImgUrl = (EditText) findViewById(R.id.etImgUrl);
+        this.etLatitude = (EditText) findViewById(R.id.etLatitude);
+        this.etLongitude = (EditText) findViewById(R.id.etLongitude);
+        this.btnUpdate = (Button) findViewById(R.id.btnUpdate);
+        this.btnDelete = (Button) findViewById(R.id.btnDelete);
+}
+}
--- app/src/main/java/com/example/fbiomateus/conhecer_vilavicosa/ReservedAreaMenu.java
+++ app/src/main/java/com/example/fbiomateus/conhecer_vilavicosa/ReservedAreaMenu.java
@@ -4,15 +4,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,11 +29,30 @@ public class ReservedAreaMenu extends AppCompatActivity {
         getSupportActionBar().setTitle("Menu Administração");
         dbHelper = new DBHelper(this);
 
-        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnAdd = (Button) findViewById(R.id.btnUpdate);
         this.btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(ReservedAreaMenu.this, AddPlace.class);
+                startActivity(intent);
+            }
+        });
+
+        listPlaces = (ListView) findViewById(R.id.listPlaces);
+
+        this.listPlaces.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(ReservedAreaMenu.this, EditPlace.class);
+                intent.putExtra("id", places.get(position).getId());
+                intent.putExtra("name", places.get(position).getName());
+                intent.putExtra("description", places.get(position).getDescription());
+                intent.putExtra("openHour", places.get(position).getOpenHour());
+                intent.putExtra("closeHour", places.get(position).getCloseHour());
+                intent.putExtra("contact", places.get(position).getContact());
+                intent.putExtra("imgUrl", places.get(position).getImgUrl());
+                intent.putExtra("latitude", places.get(position).getLatitude());
+                intent.putExtra("longitude", places.get(position).getLongitude());
                 startActivity(intent);
             }
         });
