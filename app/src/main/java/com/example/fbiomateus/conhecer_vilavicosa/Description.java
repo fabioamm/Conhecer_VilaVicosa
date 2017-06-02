8c16df707cd9c0885cb139807466f71582073361
cristianochamorra <cristiano_chamorra@hotmail.com>
2017-06-02 19:45:26 +0100

    tava dificil esta merda. Classe description - ja calcula a distancia entre a distancia actual e o ponto de interesse

--- app/src/main/java/com/example/fbiomateus/conhecer_vilavicosa/Description.java
+++ app/src/main/java/com/example/fbiomateus/conhecer_vilavicosa/Description.java
@@ -1,12 +1,19 @@
 package com.example.fbiomateus.conhecer_vilavicosa;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Location;
 import android.media.Image;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,6 +23,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.SphericalUtil;
 import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
@@ -26,14 +39,17 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Locale;
 
-public class Description extends AppCompatActivity {
+public class Description extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener,GoogleApiClient.ConnectionCallbacks,LocationListener {
 
     private Button btnDirection;
-    private TextView txtName;
-    private TextView txtDescription;
-    private TextView txtHours;
-    private TextView txtContact;
+    private TextView txtName, txtDescription,txtHours,txtContact,txtDistance;
     private ImageView imgPlace;
+    private GoogleApiClient mGoogleApiClient;
+    public Location mLastLocation;
+    private final int MY_PERMISSION_REQUEST_ID = 1234;
+    private final int MY_PERMISSION_REQUEST_ID_0 = 1234;
+    private double dist, lat, lng;
+    private LatLng pointLatLng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,12 +58,25 @@ public class Description extends AppCompatActivity {
         setContentView(R.layout.activity_description);
         findViews();
 
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
         final Intent intent = getIntent();
         getSupportActionBar().setTitle(intent.getStringExtra("name"));
         txtName.setText(intent.getStringExtra("name"));
         txtDescription.setText(intent.getStringExtra("description"));
         txtHours.setText(intent.getStringExtra("openHour") + " - " + intent.getStringExtra("closeHour"));
         txtContact.setText(intent.getStringExtra("contact"));
+        lat = Double.valueOf(intent.getStringExtra("latitude"));
+        lng = Double.valueOf(intent.getStringExtra("longitude"));
+
+        pointLatLng = new LatLng(lat,lng);
+
 
         new DownLoadImageTask(imgPlace).execute(intent.getStringExtra("imgUrl"));
 
@@ -65,6 +94,8 @@ public class Description extends AppCompatActivity {
         });
     }
 
+
+
     private void findViews() {
         this.btnDirection = (Button) findViewById(R.id.btnDirection);
         this.txtName = (TextView) findViewById(R.id.txtName);
@@ -72,6 +103,83 @@ public class Description extends AppCompatActivity {
         this.txtHours = (TextView) findViewById(R.id.txtHours);
         this.txtContact = (TextView) findViewById(R.id.txtContact);
         this.imgPlace = (ImageView) findViewById(R.id.imgPlace);
+        txtDistance = (TextView)findViewById(R.id.txtDistance);
+    }
+
+    @Override
+    public void onConnected(Bundle ConnectionHint) {
+        getLocation();
+        compareLocation();
+    }
+
+    protected void getLocation(){
+        //check permission to acess location
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        //if not allowed, get it
+        if (permissionCheck == PackageManager.PERMISSION_DENIED){
+            //ask for permission
+            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.ACCESS_FINE_LOCATION},MY_PERMISSION_REQUEST_ID);
+            return;
+        }
+
+        //check permission to acess location
+        int permissionCheck1 = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        //if not allowed, get it
+        if (permissionCheck1 == PackageManager.PERMISSION_DENIED){
+            //ask for permission
+            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.ACCESS_COARSE_LOCATION},MY_PERMISSION_REQUEST_ID_0);
+            return;
+        }
+
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+        }
+        else{
+           Log.d("Conhecer VV","Localizaçao FUck");
+        }
+
+    }
+
+        protected void compareLocation(){
+            if (mLastLocation == null){
+                Log.d("Conhecer Vila Viçosa", "currentLocation == null");
+            }else{
+                LatLng myLatLng = new LatLng(mLastLocation.getLatitude(),mLastLocation.getLongitude());
+                dist = SphericalUtil.computeDistanceBetween(myLatLng,pointLatLng);
+                txtDistance.setText(String.valueOf(dist));
+
+            }
+
+
+        }
+
+
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
     }
 
     private class DownLoadImageTask extends AsyncTask<String,Void,Bitmap> {
